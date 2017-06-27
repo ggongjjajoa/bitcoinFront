d21232faee5210c250c0582c5caf863aa2847ee7
DESKTOP-Q06MS11\lhger <lhger12@naver.com>
2017-06-28 02:36:40 +0900

    number 인풋 예외처리, order창 레이아웃 설정

---
+++ src/components/numberInput.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import TextField from 'react-md/lib/TextFields';
+
+import FontIcon from 'react-md/lib/FontIcons';
+
+class numberInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: this.props.value,
+			touchActive: false
+		}
+	}
+
+	_onTouchStart(step) {
+		this.setState({
+			touchActive: true
+		}, () => {
+			this._onPressLoop(step);
+		});
+	}
+
+	_onTouchEnd() {
+		this.setState({touchActive: false});
+	}
+
+	_onPressLoop(step) {
+		if (this.state.touchActive) {
+			if (this.props.max >= ((this.state.value * 1) + (step * 1)).toFixed(this.props.float) && ((this.state.value * 1) + (step * 1)).toFixed(this.props.float) >= this.props.min) {
+				this.setState({
+					value: ((this.state.value * 1) + (step * 1)).toFixed(this.props.float)
+				});
+				setTimeout(() => {
+					this._onPressLoop(step);
+				}, 100);
+			}
+		}
+	}
+
+	_onChange(data) {
+		if (!isNaN(data)) {
+			if (this.props.min > data) {
+				this.setState({value: this.props.min});
+			} else if (data > this.props.max) {
+				this.setState({value: this.props.max});
+			} else {
+				this.setState({value: data});
+			}
+		}else{
+            this.setState({value: this.props.min});
+        }
+	}
+
+	render() {
+		const left = <FontIcon onTouchStart= {() => {this._onTouchStart((this.props.step*-1))}} onTouchEnd= {() => {this._onTouchEnd()}}>remove</FontIcon>
+		const right = <FontIcon onTouchStart= {() => {this._onTouchStart(this.props.step)}} onTouchEnd= {() => {this._onTouchEnd()}}>add</FontIcon>
+		return (<TextField id="numberInput" floating={true} label={this.props.label} value={this.state.value} onChange={(e) => {
+			this._onChange(e);
+		}} type="number" leftIcon={left} rightIcon={right} className={this.props.className} style={{
+			margin: "0px 8px"
+		}}/>);
+	}
+}
+numberInput.defaultProps = {
+	float: 0,
+    min: 0,
+    max: 99999999,
+    step: 1,
+    value: 0
+};
+
+export default numberInput;
--- src/components/trade/Order3.js
+++ src/components/trade/Order3.js
@@ -16,129 +16,59 @@ import Tabs from 'react-md/lib/Tabs/Tabs';
 import Tab from 'react-md/lib/Tabs/Tab';
 import TabsContainer from 'react-md/lib/Tabs/TabsContainer';
 
+import NumberInput from '../numberInput';
+
+import Checkbox from 'react-md/lib/SelectionControls/Checkbox';
+
 class Order3 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            price: this.props.price,
-            amount: this.props.amount,
-            leverage: 10,
-            touchActive: false
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			price: this.props.price,
+			amount: this.props.amount,
+			leverage: 10,
+			touchActive: false
+		}
+	}
 
-    _onTouchStart(data, step) {
-        this.setState({
-            touchActive: true
-        }, () => {
-            this._onPressLoop(data, step);
-        });
-    }
-
-    _onTouchEnd() {
-        this.setState({touchActive: false});
-    }
-
-    _onPressLoop(data, step) {
-        if (this.state.touchActive) {
-            let nextState = {};
-            nextState[data] = (this.state[data]*1) + step;
-            this.setState(nextState);
-            setTimeout(() => {
-                this._onPressLoop(data, step);
-            }, 100);
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Toolbar colored title="New Order" fixed actions={< Button icon onClick = {
-                    () => {
-                        this.props.closeDialog()
-                    }
-                } > close < /Button>}/>
-                <form className="md-toolbar-relative">
-                    <TabsContainer onTabChange={(e) => {
-                        console.log(e);
-                    }}>
-                        <Tabs tabId="tab">
-                            <Tab label="Long" style={{
-                                color: "#000000"
-                            }}/>
-                            <Tab label="Short" style={{
-                                color: "#000000"
-                            }}/>
-                        </Tabs>
-                    </TabsContainer>
-                    <div className="md-grid" style={{
-                        margin: "0px 8px",
-                        padding: "0px"
-                    }}>
-                        <Switch id="marketCheck" name="market" label="Market" className="md-cell md-cell--4-phone md-cell--12 md-cell--bottom" style={{
-                            margin: "0px 8px"
-                        }}/>
-                    <TextField id="PriceField" placeholder="Price" value={this.state.price} onChange={(e)=>{this.setState({price:e})}} type="number" leftIcon={< FontIcon onTouchStart = {
-                            () => {
-                                this._onTouchStart("price", -0.1)
-                            }
-                        }
-                        onTouchEnd = {
-                            () => {
-                                this._onTouchEnd()
-                            }
-                        } > remove < /FontIcon>} rightIcon={< FontIcon onTouchStart = {
-                            () => {
-                                this._onTouchStart("price", 0.1)
-                            }
-                        }
-                        onTouchEnd = {
-                            () => {
-                                this._onTouchEnd()
-                            }
-                        } > add < /FontIcon>} className="md-cell md-cell--4-phone md-cell--4 md-cell--bottom" style={{
-                            margin: "0px 8px"
-                        }}/>
-                        <TextField id="AmountField" placeholder="Amount" value={this.state.amount} type="number" leftIcon={< FontIcon onTouchStart = {
-                            () => {
-                                this._onTouchStart("amount", -1)
-                            }
-                        }
-                        onTouchEnd = {
-                            () => {
-                                this._onTouchEnd()
-                            }
-                        } > remove < /FontIcon>} rightIcon={< div > < FontIcon > vertical_align_top < /FontIcon > < FontIcon onTouchStart = { () => { this._onTouchStart("amount", 1) } } onTouchEnd = { () => { this._onTouchEnd() } } > add < /FontIcon > </div>} className="md-cell md-cell--4-phone md-cell--4 md-cell--bottom" style={{
-                            margin: "0px 8px"
-                        }}/>
-                        <TextField placeholder="Leverage" id="LeverageField" value={this.state.leverage} type="number" leftIcon={< FontIcon onTouchStart = {
-                            () => {
-                                this._onTouchStart("leverage", -1)
-                            }
-                        }
-                        onTouchEnd = {
-                            () => {
-                                this._onTouchEnd()
-                            }
-                        } > remove < /FontIcon>} rightIcon={< FontIcon onTouchStart = {
-                            () => {
-                                this._onTouchStart("leverage", 1)
-                            }
-                        }
-                        onTouchEnd = {
-                            () => {
-                                this._onTouchEnd()
-                            }
-                        } > add < /FontIcon>} className="md-cell md-cell--4-phone md-cell--4 md-cell--bottom" style={{
-                            margin: "0px 8px"
-                        }}/>
-                        <Button primary raised label="Chat" className="md-cell md-cell--2-phone md-cell--2 md-cell--8-offset">chat_bubble_outline</Button>
-                        <Button secondary raised label="Chat" className="md-cell md-cell--2-phone md-cell--2">chat_bubble_outline</Button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Toolbar colored title="New Order" fixed actions={< Button icon onClick = {
+					() => {
+						this.props.closeDialog()
+					}
+				} > close < /Button>}/>
+				<form className="md-toolbar-relative">
+					<TabsContainer onTabChange={(e) => {
+						console.log(e);
+					}}>
+						<Tabs tabId="tab">
+							<Tab label="Long" style={{
+								color: "#000000"
+							}}/>
+							<Tab label="Short" style={{
+								color: "#000000"
+							}}/>
+						</Tabs>
+					</TabsContainer>
+					<div className="md-grid" style={{
+						margin: "0px 0px",
+						padding: "0px"
+					}}>
+                        <NumberInput value={0} step={0.1} label={"Price"} float={2} className="md-cell md-cell--2-phone md-cell--3 md-cell--bottom"/>
+						<Checkbox id="market" name="marketCheck" label="Market" className="md-cell md-cell--2-phone md-cell--1 md-cell--bottom"/>
+						<NumberInput value={0} label={"Amount"} className="md-cell md-cell--2-phone md-cell--3 md-cell--bottom"/>
+                        <Button raised label="Max" className="md-cell md-cell--2-phone md-cell--1 md-cell--bottom"/>
+						<NumberInput value={10} min={1} max={80} label={"Leveage"} className="md-cell md-cell--2-phone md-cell--3 md-cell--bottom"/>
+                        <div className="md-cell md-cell--2-phone md-cell--1 md-cell--bottom"/>
+						<Button primary raised label="Chat" className="md-cell md-cell--2-phone md-cell--2 md-cell--8-offset"></Button>
+						<Button secondary raised label="Chat" className="md-cell md-cell--2-phone md-cell--2"></Button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Order3;
